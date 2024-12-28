e555061e2854d0d9c264f727499046d789d61a6e
miro <jarbasai@mailfence.com>
2024-12-28 00:27:37 +0000

    refactor!: uncouple agent and network protocol from hivemind protocol

--- hivemind_core/__main__.py
+++ hivemind_core/__main__.py
@@ -1,8 +1,11 @@
 from hivemind_core.service import HiveMindService
-
+from hivemind_core.agents import OVOSProtocol
+from hivemind_core.server import HiveMindWebsocketProtocol
 
 def main():
-    service = HiveMindService()
+
+    service = HiveMindService(agent_protocol=OVOSProtocol,
+                              network_protocol=HiveMindWebsocketProtocol)
     service.run()
 
 
