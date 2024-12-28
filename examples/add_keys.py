3171ef1876de6779a04c139455db44984d28955a
miro <jarbasai@mailfence.com>
2024-12-28 16:42:47 +0000

    rm outdated examples

--- examples/add_keys.py
+++
@@ -1,9 +0,0 @@
-from hivemind_core.database import ClientDatabase
-
-name = "JarbasCliTerminal"
-key = "RESISTENCEisFUTILE"
-crypto_key = "resistanceISfutile"
-
-
-with ClientDatabase() as db:
-    db.add_client(name, key, crypto_key=crypto_key)
