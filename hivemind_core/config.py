f484fac43dcec3262e8614b737a1414d2bafd867
miro <jarbasai@mailfence.com>
2025-01-05 20:49:52 +0000

    fix: track last_seen , add missing cli permission management scripts , fix broadcast permission

--- hivemind_core/config.py
+++ hivemind_core/config.py
@@ -9,7 +9,8 @@ _DEFAULT = {
     "binarize": False,  # default False because of a bug in old hivemind-bus-client versions
 
     # sort encodings by order of preference
-    "allowed_encodings": ["JSON-B64", "JSON-URLSAFE-B64",
+    "allowed_encodings": ["JSON-B64",
+                          "JSON-URLSAFE-B64",
                           "JSON-B91",
                           "JSON-Z85B", "JSON-Z85P",
                           "JSON-B32", "JSON-HEX"],
@@ -22,16 +23,10 @@ _DEFAULT = {
                            "port": 8181
                        }},
     "binary_protocol": {"module": None},
-    "network_protocol": {"hivemind-websocket-plugin": {
+    "network_protocol": {"module": "hivemind-websocket-plugin",
+                         "hivemind-websocket-plugin": {
                              "host": "0.0.0.0",
                              "port": 5678,
-                             "ssl": False,
-                             "cert_dir": f"{xdg_data_home()}/hivemind",
-                             "cert_name": "hivemind"
-                         },
-                         "hivemind-http-plugin": {
-                             "host": "0.0.0.0",
-                             "port": 5679,
                              "ssl": False,
                              "cert_dir": f"{xdg_data_home()}/hivemind",
                              "cert_name": "hivemind"
@@ -50,7 +45,6 @@ def get_server_config() -> JsonStorageXDG:
     if not os.path.isfile(db.path):
         db.merge(_DEFAULT)
         db.store()
-    # ensure all top level keys are present
     for k, v in _DEFAULT.items():
         if k not in db:
             db[k] = v
