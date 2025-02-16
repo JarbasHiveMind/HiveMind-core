d7d29f76b2793da8e221e46fe7b4381d68ae1774
miro <jarbasai@mailfence.com>
2025-02-16 00:16:33 +0000

    log to file `~/.local/state/hivemind/core.log`

--- hivemind_core/__main__.py
+++ hivemind_core/__main__.py
@@ -1,7 +1,15 @@
 from hivemind_core.service import HiveMindService
+from ovos_utils.log import init_service_logger, LOG
+import os
+
+from ovos_utils.xdg_utils import xdg_state_home
+
+from ovos_config.meta import get_xdg_base
+
 
 def main():
-
+    init_service_logger("core")
+    LOG.base_path = os.path.join(xdg_state_home(), "hivemind")
     service = HiveMindService()
     service.run()
 
